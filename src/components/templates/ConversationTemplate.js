8fc64ea25597d647c4b01f865364fd3518175e2c
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-26 10:15:41 -0300

    create all template files

---
+++ src/components/templates/ConversationTemplate.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ConversationTemplate() {
+  return <div></div>
+}
---
+++ src/components/templates/LoginTemplate.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function LoginTemplate() {
+  return <div></div>
+}
