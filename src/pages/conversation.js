0d45dc00ce8cae17b458232b69f71d4e88e29c1d
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-26 10:02:11 -0300

    create pages files

---
+++ src/pages/conversation.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Conversation() {
+  return <div></div>
+}
---
+++ src/pages/profile-likes.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ProfileLikes() {
+  return <div></div>
+}
