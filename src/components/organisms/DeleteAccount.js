3f8fa9478e35bf893fca5ae8168d00940a2100dc
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-02 15:24:44 -0300

    finish settings page, only need logic for deleting the user

--- src/components/organisms/DeleteAccount.js
+++ src/components/organisms/DeleteAccount.js
@@ -1,13 +1,70 @@
+import { SettingsButton } from '../atoms'
 import { SimpleHeader } from '../molecules'
 import * as ROUTES from '../../constants/routes'
+import styled from 'styled-components'
 
-export default function DeleteAccount() {
+const Inner = styled.div`
+  .delete-texts-container {
+    box-sizing: border-box;
+    padding: 0 1rem;
+
+    h2,
+    h3,
+    h4 {
+      color: var(--xDarkGrey);
+    }
+
+    h2 {
+      font-size: 20px;
+    }
+
+    h3,
+    h4 {
+      font-size: 18px;
+    }
+
+    ul {
+      color: var(--darkGrey);
+      font-size: 17px;
+      list-style: square;
+    }
+  }
+
+  .final-delete-button-container {
+    height: 4rem;
+  }
+`
+
+export default function DeleteAccount({ user }) {
+  const handleDelete = () => {}
+
   return (
     <div>
       <SimpleHeader withArrow={true} arrowLink={ROUTES.SETTINGS}>
         Delete Account
       </SimpleHeader>
-      delete
+      <Inner>
+        <div className="delete-texts-container">
+          <h2>Are you sure you want to delete your account?</h2>
+
+          <h3>If you delete you account:</h3>
+
+          <ul>
+            <li>All your posts will be gone.</li>
+            <li>All your comments will be gone.</li>
+            <li>All your messages will be gone.</li>
+            <li>All your likes will be gone.</li>
+          </ul>
+
+          <h4>There's no way back</h4>
+        </div>
+
+        <div onClick={handleDelete} className="final-delete-button-container">
+          <SettingsButton isDelete={true}>
+            I'm sure I want to delete my account!
+          </SettingsButton>
+        </div>
+      </Inner>
     </div>
   )
 }
