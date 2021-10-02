3ec7ba4b671ef81c01fdde61fa28051d84c9e8ff
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-02 15:27:01 -0300

    add header to homepage

--- src/components/templates/HomeTemplate.js
+++ src/components/templates/HomeTemplate.js
@@ -2,11 +2,13 @@ import { useContext } from 'react'
 import UserContext from '../../context/userContext'
 import { LoggedInLayout } from '../layouts'
 import { Timeline, PostBox } from '../organisms'
+import { SimpleHeader } from '../molecules'
 
 export default function HomeTemplate() {
   const { user } = useContext(UserContext)
   return (
     <LoggedInLayout user={user} showSearchBar={true} showSuggestion={true}>
+      <SimpleHeader>Homepage</SimpleHeader>
       <PostBox user={user} />
       <Timeline user={user} />
     </LoggedInLayout>
