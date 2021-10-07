f4d7c494b153be668a3c65fc9e7aae6dc5a9d60e
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-07 16:02:44 -0300

    set up files for profile page

---
+++ src/components/organisms/ProfileHeader.js
@@ -0,0 +1,3 @@
+export default function ProfileHeader() {
+  return <div></div>
+}
--- src/components/organisms/ProfileOrg.js
+++ src/components/organisms/ProfileOrg.js
@@ -1,3 +1,9 @@
+import { LoggedUserAvatar } from '../atoms'
+
 export default function ProfileOrg({ user, posts }) {
-  return <div></div>
+  return (
+    <div>
+      <LoggedUserAvatar size="extra-larger" user={user} />
+    </div>
+  )
 }
