830849f2fd2dbd032f2422a5e64605a95e87aaa9
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-07 15:53:14 -0300

    create hook to get a user's posts for his profile

---
+++ src/hooks/useProfilePosts.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { getProfilePosts } from '../services/postServices'
+
+export default function useProfilePosts(userId) {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await getProfilePosts(userId)
+
+      setPosts(response)
+    }
+
+    if (userId) getPosts()
+  }, [userId])
+
+  return { posts }
+}
