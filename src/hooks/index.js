4af3ae5fbedc00aa4a9d285217d7d910811df43a
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-08 11:00:20 -0300

    add new hook to treeshaking

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useAuthListener } from './useAuthListener'
 export { default as useProfilePosts } from './useProfilePosts'
+export { default as useFollowedPosts } from './useFollowedPosts'
