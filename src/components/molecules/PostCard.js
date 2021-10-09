06c9b5b197b3e690b8d99dacb7a7c5540ae81071
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-09 17:20:05 -0300

    create Post Card comp and add its style and logic

---
+++ src/components/molecules/PostCard.js
@@ -0,0 +1,270 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { parseTimestamp } from '../../utils/parseTimestamp'
+import { Dot } from '../atoms'
+import { FaRegComment, FaRegHeart, FaHeart, FaRetweet } from 'react-icons/fa'
+import { useContext, useEffect, useState } from 'react'
+import userContext from '../../context/userContext'
+import { ToggleInteraction } from '../../services/postServices'
+
+const Container = styled.article`
+  display: grid;
+  grid-template-columns: 50px auto;
+  border-bottom: 1px solid var(--xLightGrey);
+  box-sizing: border-box;
+  padding: 1rem 0.5rem 0.2rem 0.5rem;
+
+  .post-img-container {
+    width: 50px;
+    height: 50px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 50%;
+    }
+  }
+
+  .post-rest-container {
+    box-sizing: border-box;
+    padding: 0 1rem;
+
+    .post-top-info {
+      display: flex;
+      align-items: center;
+      gap: 0.3rem;
+
+      a {
+        display: inherit;
+        gap: inherit;
+        align-items: inherit;
+        text-decoration: none;
+      }
+
+      .post-creator-info {
+        display: inherit;
+        gap: inherit;
+        align-items: inherit;
+        cursor: pointer;
+
+        &:hover > h1 {
+          text-decoration: underline;
+        }
+      }
+
+      h1 {
+        font-size: 17px;
+        color: var(--black);
+      }
+
+      span,
+      p {
+        font-size: 15px;
+        color: var(--darkGrey);
+      }
+
+      h1,
+      p {
+        margin: 0;
+      }
+    }
+
+    .post-content-container {
+      p {
+        font-size: 15px;
+        text-align: justify;
+        margin: 0.5rem 0;
+      }
+    }
+
+    .post-interactions-container {
+      display: flex;
+      gap: 8rem;
+      align-items: center;
+
+      .interaction-container {
+        display: flex;
+        align-items: center;
+        gap: 0.3rem;
+        color: var(--darkGrey);
+        cursor: pointer;
+
+        .icon-container {
+          padding: 0.5rem 0.6rem;
+          border-radius: 50%;
+          box-sizing: border-box;
+          transition: 400ms ease;
+        }
+
+        p {
+          margin: 0;
+          font-size: 13.5px;
+          transition: 400ms ease;
+          min-width: 1rem;
+        }
+
+        &.comment {
+          &:hover > .icon-container {
+            color: var(--primary);
+            background-color: var(--xLightGrey);
+          }
+
+          &:hover > p {
+            color: var(--primary);
+          }
+        }
+
+        &.re-post {
+          &:hover > .icon-container {
+            color: var(--success);
+            background-color: #c8f7c8;
+          }
+
+          &:hover > p {
+            color: var(--success);
+          }
+        }
+
+        &.like {
+          .icon-container {
+            padding: 0.4rem 0.6rem;
+          }
+          svg {
+            margin-top: 0.2rem;
+          }
+
+          &:hover > .icon-container {
+            color: var(--error);
+            background-color: var(--errorLight);
+          }
+
+          &:hover > p {
+            color: var(--error);
+          }
+        }
+
+        &.re-posted {
+          color: var(--success);
+        }
+
+        &.liked {
+          color: var(--error);
+        }
+      }
+    }
+  }
+`
+
+export default function PostCard({ post }) {
+  const [isLiked, setIsLiked] = useState(false)
+  const [likeCount, setLikeCount] = useState(0)
+
+  const [isRePosted, setIsRePosted] = useState(false)
+  const [rePostCount, setRePostCount] = useState(0)
+
+  const { user } = useContext(userContext)
+
+  const handleLikeToggle = async () => {
+    await ToggleInteraction(post.docId, 'likes', isLiked, setIsLiked, user.id)
+  }
+
+  const handleRePostToggle = async () => {
+    await ToggleInteraction(
+      post.docId,
+      'rePosts',
+      isRePosted,
+      setIsRePosted,
+      user.id
+    )
+  }
+
+  useEffect(() => {
+    if (post.likes.includes(user.id)) {
+      setIsLiked(true)
+    }
+  }, [post.likes, user.id])
+
+  useEffect(() => {
+    if (post.rePosts.includes(user.id)) {
+      setIsRePosted(true)
+    }
+  }, [post.rePosts, user.id])
+
+  useEffect(() => {
+    setLikeCount((prev) => (isLiked ? prev + 1 : prev !== 0 ? prev - 1 : 0))
+  }, [isLiked])
+
+  useEffect(() => {
+    setRePostCount((prev) =>
+      isRePosted ? prev + 1 : prev !== 0 ? prev - 1 : 0
+    )
+  }, [isRePosted])
+
+  return (
+    <Container>
+      <div className="post-img-container">
+        <Link to={`/p/${post.creatorUsername}`}>
+          <img
+            src={`/images/avatars/${
+              post.creatorAvatar + '.jpg' || 'default-avatar.png'
+            }`}
+            alt={post.creatorName}
+          />
+        </Link>
+      </div>
+
+      <div className="post-rest-container">
+        <div className="post-top-info">
+          <Link to={`/p/${post.creatorUsername}`}>
+            <div className="post-creator-info">
+              <h1>{post.creatorName}</h1>
+
+              <span>@{post.creatorUsername}</span>
+            </div>
+          </Link>
+
+          <Dot />
+
+          <p>{parseTimestamp(post.createdAt)}</p>
+        </div>
+
+        <div className="post-content-container">
+          <p>{post.content}</p>
+        </div>
+
+        <div className="post-interactions-container">
+          <div className="interaction-container comment">
+            <div className="icon-container">
+              <FaRegComment />
+            </div>
+
+            <p>1</p>
+          </div>
+
+          <div
+            onClick={handleRePostToggle}
+            className={`interaction-container re-post ${
+              isRePosted ? 're-posted' : ''
+            }`}
+          >
+            <div className="icon-container">
+              <FaRetweet />
+            </div>
+
+            <p>{rePostCount > 0 && rePostCount}</p>
+          </div>
+
+          <div
+            onClick={handleLikeToggle}
+            className={`interaction-container like ${isLiked ? 'liked' : ''}`}
+          >
+            <div className="icon-container">
+              {isLiked ? <FaHeart /> : <FaRegHeart />}
+            </div>
+            <p>{likeCount > 0 && likeCount}</p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
