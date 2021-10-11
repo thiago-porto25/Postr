b472c4ddbe7655f0b4c95d3f8dbe3bf8a19b9b98
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-11 12:08:21 -0300

    create files to help with profile edit component

---
+++ src/utils/avatars.js
@@ -0,0 +1,12 @@
+const avatarsData = [
+  'avatar1',
+  'avatar2',
+  'avatar3',
+  'avatar4',
+  'avatar5',
+  'avatar6',
+  'avatar7',
+  'avatar8',
+]
+
+export default avatarsData
---
+++ src/utils/backgrounds.js
@@ -0,0 +1,3 @@
+const backgroundData = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7', 'bg8']
+
+export default backgroundData
