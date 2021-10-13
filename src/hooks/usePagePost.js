1a3730a9c142d97f92e6acdd92083dc3c69001d1
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-13 20:02:06 -0300

    create hook to use the post of a post page

---
+++ src/hooks/usePagePost.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { getPostById } from '../services/postServices'
+
+export default function useFollowedPosts(postId) {
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await getPostById(postId)
+
+      setPost(response)
+    }
+
+    if (postId) getPosts()
+  }, [postId])
+
+  return { post }
+}
