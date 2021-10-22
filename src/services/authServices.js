30d8f2056337ab454185bba902d2313931c7c20a
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-22 19:01:28 -0300

    fix bug with document ref in the delete User Service

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -104,7 +104,7 @@ export const signupWithFirebase = async ({
 export const deleteUserFromDb = async (user) => {
   try {
     await deleteUser(user)
-    await deleteDoc(db, 'users', user.uid)
+    await deleteDoc(doc(db, 'users', user.uid))
     await deleteUserFromFollow(user.uid, 'followers')
     await deleteUserFromFollow(user.uid, 'following')
     await deleteUserInteractions(user.uid, 'likes')
